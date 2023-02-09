dfdaec0e247f7e8d767e4354489b603b88bbfc23
riquew <hwindlin@gmail.com>
2023-02-09 15:32:03 -0300

    Modulariza os scripts

---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initTabNav from "./modules/tab-nav.js";
+import initAccordion from "./modules/accordion.js";
+import initScrollSuave from "./modules/scrollSuave.js";
+import initAnimacaoScroll from "./modules/scrollNav.js";
+
+initTabNav();
+initAccordion();
+initScrollSuave();
+initAnimacaoScroll();
