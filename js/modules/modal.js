0aebadc157f815ef6dcce8f685a218acd876b17b
riquew <hwindlin@gmail.com>
2023-02-10 10:37:10 -0300

    Adiciona tooltip ao mapa

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,18 +1,20 @@
 export default function initModal() {
   const modalOpen = document.querySelector("[data-modal='abrir']");
   const modalClose = document.querySelector("[data-modal='fechar']");
-  const modalForm = document.querySelector(".modal-container");
+  const containerModal = document.querySelector(".modal-container");
 
-  function modalOn(event) {
-    event.preventDefault();
-    modalForm.classList.add("ativo");
+  if (modalOpen && modalClose && containerModal) {
+    function toggleModal(event) {
+      event.preventDefault();
+      containerModal.classList.toggle("ativo");
+    }
+
+    function cliqueForaModal(event) {
+      if (event.target === this) toggleModal(event);
+    }
+
+    modalOpen.addEventListener("click", toggleModal);
+    modalClose.addEventListener("click", toggleModal);
+    containerModal.addEventListener("click", cliqueForaModal);
   }
-
-  function modalOff(event) {
-    event.preventDefault();
-    modalForm.classList.remove("ativo");
-  }
-
-  modalOpen.addEventListener("click", modalOn);
-  modalClose.addEventListener("click", modalOff);
 }
--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAccordion from "./modules/accordion.js";
 import initScrollSuave from "./modules/scrollSuave.js";
 import initAnimacaoScroll from "./modules/scrollNav.js";
 import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
 
 initTabNav();
 initAccordion();
 initScrollSuave();
 initAnimacaoScroll();
 initModal();
+initTooltip();
