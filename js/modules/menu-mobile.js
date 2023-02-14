c02c9f1ef2cc81e4ec70b678cd0b3a9fd4221862
riquew <hwindlin@gmail.com>
2023-02-14 16:22:54 -0300

    Adiciona animacao aos numeros

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,15 +1,22 @@
 import outsideClick from "./outsideclick.js";
 
 export default function initMenuMobile() {
-  console.log("menuMobile");
+  const menuButton = document.querySelector("[data-menu='button']");
+  const menuList = document.querySelector("[data-menu='list']");
+  const eventos = ["click", "touchstart"];
+
+  if (menuButton) {
+    function openMenu(event) {
+      menuButton.classList.add("active");
+      menuList.classList.add("active");
+      outsideClick(menuList, eventos, () => {
+        menuButton.classList.remove("active");
+        menuList.classList.remove("active");
+      });
+    }
+
+    eventos.forEach((evento) => {
+      menuButton.addEventListener(evento, openMenu);
+    });
+  }
 }
-
-const menuButton = document.querySelector("[data-menu='button']");
-const menuList = document.querySelector("[data-menu='list']");
-
-function openMenu(event) {
-  menuButton.classList.toggle("active");
-  menuList.classList.toggle("active");
-}
-
-menuButton.addEventListener("click", openMenu);
