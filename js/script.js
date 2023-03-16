1447056fd4535f2655abd48f70a55b2d1e3a95d9
riquew <hwindlin@gmail.com>
2023-03-16 16:31:18 -0300

    ScrollSuave transformado em classe

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import initTabNav from "./modules/tab-nav.js";
 import initAccordion from "./modules/accordion.js";
-import initScrollSuave from "./modules/scrollSuave.js";
+import ScrollSuave from "./modules/scrollSuave.js";
 import initAnimacaoScroll from "./modules/scrollNav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
@@ -10,9 +10,11 @@ import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitCoin from "./modules/fetch-bitcoin.js";
 
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
+
 initTabNav();
 initAccordion();
-initScrollSuave();
 initAnimacaoScroll();
 initModal();
 initTooltip();
